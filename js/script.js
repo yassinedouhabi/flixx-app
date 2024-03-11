6efbd34defc55f9e487dc1341e48fad33d707c6b
Yassine Douhabi <douhabiyassine@gmail.com>
2024-03-11 19:26:59 +0000

    commit from MacOS

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+const base_url = 'https://image.tmdb.org/t/p/';
+const file_size = 'w500';
+const posterURL = `${base_url}${file_size}`;
+
 // High Light Active Link
 function highLightActiveLink() {
   const pathLocation = window.location.pathname;
@@ -17,7 +21,65 @@ function highLightActiveLink() {
 // Get Popular Movies
 async function displayPopularMovies() {
   const { results } = await fetchAPIData('movie/popular');
+  const popularMoviesContainer = document.getElementById('popular-movies');
+  results.forEach((movie) => {
+    const movieCard = document.querySelector('.card');
+    movieCard.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+    <img
+      src="${posterURL}${movie.poster_path}"
+      class="card-img-top"
+      alt="${movie.title}"
+    />
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${movie.title}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${movie.release_date}</small>
+      </p>
+    </div>
+    `;
+    popularMoviesContainer.appendChild(movieCard);
+  });
+}
+
+// Display Movie Details
+async function displayMovieDetails() {
+  const currentUrl = window.location.href;
+  // Create URL object
+  const urlObject = new URL(currentUrl);
+  // Use URLSearchParams to get query parameters
+  const queryParams = new URLSearchParams(urlObject.search);
+  // Access the 'id' parameter
+  const movieId = queryParams.get('id');
+
+  console.log(movieId);
+}
+
+// Get Popular TV Shows
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular');
   console.log(results);
+  const popularShowsContainer = document.getElementById('popular-shows');
+  results.forEach((show) => {
+    const showCard = document.querySelector('.card');
+    showCard.innerHTML = `
+    <a href="tv-details.html?id=${show.id}">
+    <img
+      src="${posterURL}${show.poster_path}"
+      class="card-img-top"
+      alt="${show.name}"
+    />
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${show.name}</h5>
+      <p class="card-text">
+         <small class="text-muted">Aired: ${show.first_air_date}</small>
+      </p>
+    </div>
+    `;
+    popularShowsContainer.appendChild(showCard);
+  });
 }
 
 // Fetch Data From TMDB API
@@ -39,9 +101,10 @@ function init() {
       break;
     case '/shows.html':
       console.log('shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
-      console.log('movie-details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('tv-details');
